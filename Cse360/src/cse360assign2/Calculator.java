f5d42fa0bc991f479d8212d442969f1a72896eb0
Suraj Atmakuri <surajatmakuri@hotmail.com>
2019-02-24 13:26:49 -0700

    pushing files

---
+++ Cse360/src/cse360assign2/Calculator.java
@@ -0,0 +1,76 @@
+package cse360assign3;
+/**
+* Program to implement a basic calculator
+* @author Suraj Atmakuri 
+* Class-ID: 320
+*/
+/*
+ * Class Calculator can perform all the basic operations
+ * and can also allows the user to view history 
+*/
+public class Calculator 
+{
+
+	private int total;
+	
+	/** 
+	    * Class constructor.
+	    */
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	/** 
+	    * Returns the total after performing the operations for the input
+	    * Can only be called if there is a '=' symbol in the input
+	    */
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	/** 
+	    * Adds two numbers entered by the user
+	    * @param value the number inserted by the user to perform 
+	    * the desired operation
+	    */
+	public void add (int value) 
+	{
+		
+	}
+	/** 
+	    * Subtracts numbers entered by the user
+	    * @param value the number inserted by the user to perform 
+	    * the desired operation
+	    */
+	public void subtract (int value) 
+	{
+		
+	}
+	/** 
+	    * Multiplies numbers entered by the user
+	    * @param value the number inserted by the user to perform 
+	    * the desired operation
+	    */
+	public void multiply (int value) 
+	{
+		
+	}
+	/** 
+	    * Divides numbers entered by the user
+	    * @param value the number inserted by the user to perform 
+	    * 
+	    */
+	public void divide (int value)
+	{
+		
+	}
+	/** 
+	    * Displays all the previous operations performed by the user
+	    */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
